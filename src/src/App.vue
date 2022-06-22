4ee5e7b8ddb24c2dfa71edf181702ccf508a762a
Jamie Sykes <jaymehsykes@gmail.com>
2022-06-22 22:29:01 +0100

    Adds Fabric library and plays with canvas functions.

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AppCanvas />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppCanvas from './components/AppCanvas.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppCanvas,
   }
 }
 </script>
---
+++ src/src/components/AppCanvas.vue
@@ -0,0 +1,15 @@
+<template>
+    <canvas id="c"></canvas>
+</template>
+
+<script>
+    import { fabric } from 'fabric';
+    export default {
+        name: 'AppCanvas',
+        mounted() {
+            var canvas = new fabric.Canvas('c');
+            var rectangle = new fabric.Rect({width: 20, height: 20, fill: 'red', left: 10, top: 100, selectable: true});
+            canvas.add(rectangle);
+        },
+    }
+</script>
