ac5692f1f6027aa2db6e8fe08e6078c1126b90f4
Jamie Sykes <jaymeh@users.noreply.github.com>
2022-06-22 22:46:26 +0100

    Updates something to test GPG.

--- src/src/components/AppCanvas.vue
+++ src/src/components/AppCanvas.vue
@@ -10,6 +10,7 @@
             var canvas = new fabric.Canvas('c');
             var rectangle = new fabric.Rect({width: 20, height: 20, fill: 'red', left: 10, top: 100, selectable: true});
             canvas.add(rectangle);
+            
         },
     }
 </script>
